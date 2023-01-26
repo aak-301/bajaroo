ea63060f336b2935581327254d7c6c87bb7ee2f0
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-26 10:10:32 +0530

    added listeners for like unlike

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -1,7 +1,7 @@
 import 'package:bajaroo/widgets/product_item.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/products.dart';
+import '../providers/product.dart';
 
 class ProductsGrid extends StatelessWidget {
   const ProductsGrid({
@@ -22,10 +22,9 @@ class ProductsGrid extends StatelessWidget {
           mainAxisSpacing: 10,
         ),
         itemBuilder: ((ctx, i) {
-          return ProductItem(
-            id: productData.items[i].id,
-            title: productData.items[i].title,
-            imageUrl: productData.items[i].imageUrl,
+          return ChangeNotifierProvider.value(
+            value: productData.items[i],
+            child: const ProductItem(),
           );
         }));
   }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,32 +1,37 @@
 import 'package:bajaroo/constants/routes.dart';
 import 'package:bajaroo/constants/themes.dart';
+import 'package:bajaroo/providers/products.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProductItem extends StatelessWidget {
   const ProductItem({
     super.key,
-    required this.id,
-    required this.title,
-    required this.imageUrl,
+    // required this.id,
+    // required this.title,
+    // required this.imageUrl,
   });
 
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: const Icon(
-              Icons.favorite,
+            icon: Icon(
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
               color: AppColors.iconColor,
             ),
-            onPressed: () {},
+            onPressed: () {
+              product.toggleFavStatus();
+            },
           ),
           trailing: IconButton(
             onPressed: () {},
@@ -36,15 +41,15 @@ class ProductItem extends StatelessWidget {
             ),
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
         ),
         child: GestureDetector(
           onTap: () => Navigator.of(context)
-              .pushNamed(AppRoutes.productDetails, arguments: id),
+              .pushNamed(AppRoutes.productDetails, arguments: product.id),
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
