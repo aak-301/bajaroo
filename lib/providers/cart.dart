1a69449775283e01b7d46dba34acb6d9019bbf04
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-26 15:25:23 +0530

    added cart provider class for logic

---
+++ lib/providers/cart.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem(
+      {required this.id,
+      required this.price,
+      required this.quantity,
+      required this.title});
+}
+
+class Cart with ChangeNotifier {
+  late Map<String, CartItem> _items;
+
+  Map<String, CartItem> get item {
+    return {..._items};
+  }
+
+  void addItem({
+    required String productId,
+    required double price,
+    required String title,
+  }) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+              id: existingCartItem.id,
+              price: existingCartItem.price,
+              quantity: existingCartItem.quantity + 1,
+              title: existingCartItem.title));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              price: price,
+              quantity: 1,
+              title: title));
+    }
+  }
+}
