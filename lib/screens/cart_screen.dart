477f01259d88b4134c261bd55a6dfac33d93b5d9
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-26 16:19:45 +0530

    added cart screen

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,42 @@
+import 'package:bajaroo/providers/cart.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Cart'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  const Text(
+                    'total',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Chip(
+                    label: Text('₹ ${cart.totalAmt}'),
+                    backgroundColor: Colors.purple,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
