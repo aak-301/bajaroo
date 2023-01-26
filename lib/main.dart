8418b91caceeaf4d52b252b1cf2e407665553099
aakash-301 <aakashshivanshu5@gmail.com>
2023-01-26 15:30:27 +0530

    added MultiProvider for Products and Cart Provider classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bajaroo/constants/routes.dart';
+import 'package:bajaroo/providers/cart.dart';
 import 'package:bajaroo/providers/products.dart';
 import 'package:bajaroo/screens/product_details_screen.dart';
 import 'package:bajaroo/screens/products_overview_screen.dart';
@@ -14,8 +15,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
